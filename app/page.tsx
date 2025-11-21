f56a68ec9c9e6c415b9a85d4bf2739405babaf02
lelouchZhang <zhch8128@gmail.com>
2025-11-22 00:39:00 +0800

    Bug Fixed(When downgrade prisma) & Implement tRpc

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import prisma from "@/lib/db"
+import { caller } from "@/trpc/server"
 
-const page = () => {
+const page = async () => {
+
+  // const users = await prisma.user.findMany();
+
+  const users = await caller.getUsers();
   return (
-    <div className="text-red-500">page</div>
+    <div className="text-red-500">
+      {JSON.stringify(users)}
+    </div>
   )
 }
 
